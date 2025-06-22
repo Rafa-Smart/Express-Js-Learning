e21de1d65d88ae7531253fe43c9ec410b9f1366e
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-06-22 15:09:40 +0700

    router-test

--- test/25.router.test.js
+++ test/25.router.test.js
@@ -9,7 +9,7 @@
 
 
 
-import express from 'express';
+import express, { Router } from 'express';
 import request from 'supertest';
 console.clear()
 
@@ -26,7 +26,7 @@ const router = express.Router();
 // khusus nutk yg route /rafa
 
 router.use('/rafa', (req, res, next) => {
-    res.set({"content-type":'text/plain'})
+    res.set({"content-type":'application/json'})
     console.log(`haloo ${req.query.nama} ini adalah middleware`)
     next()
 })
@@ -39,12 +39,44 @@ router.use('/rafa', (req, res, next) => {
 // router ini tidka akna jalan
 
 router.get('/rafa', (req,res) => {
-    res.send(`halo ${req.query.nama}`)
+    res.json({nama:req.query.nama})
 })
 
+// kit abisa menmbahkan settig ini untu route apa angsung di router.get('/rafa)
+// dan router.use('/rafa)
+
+// atau langusng di app.use nya kita masukan
+// app.use('/rafa', router)
 
 // cara menambahkannya
 app.use(router)
+
+
+// ini yg keduanya
+
+
+const router2 = express.Router();
+
+router2.use((req,res,next) => {
+    res.set({"content-type":'text/plain'})
+    // disini kita buat properti baru di reqnya
+    req.nama = "jamaluddin"
+    req.umur = 15
+    console.log("sudah emngakses router2 dengna /jamal")
+    next()
+})
+
+router2.get('/jamal', (req, res) => {
+    // ngambil dari si req yg sudah dimanupulasi di middleware
+    // dan middleware iniberlaku hanya untuk route /jamal
+    res.send(`halo ${req.nama}`)
+})
+
+// app.use("/jamal",router2)
+// nah itu seharusnya gausah, jadi pake cara pertama aja
+// jadi kita atur routernya langsung dari router.use("/jamal",())
+// jadi jangan di app.use("/jamal")
+app.use(router2)
 
 // maka akan aktif jika di route /rafa saja
 
@@ -57,6 +89,36 @@ app.use(router)
 test("testing 1...", async () => {
     const response = await request(app).get("/rafa").query({nama:"rafa"})
     expect(response.status).toBe(200)
-    expect(response.text).toBe('halo rafa')
+    expect(response.body).toEqual({nama:"rafa"})
+    expect(response.get("content-type")).toContain('application/json')
+
+
+    // dna disini kita cek bahwa middleware yg adadi route2 (/jamal)
+    // tidak berfungsi di route /rafa
+
+    // nah kalo jamal itu kan content-typenya text/plain
+    // maka harusnya ga bisa
+
+    expect(response.get('content-type')).not.toContain("text/plain")
+})
+test("testing 2...", async () => {
+    const response = await request(app).get("/jamal")
+    expect(response.status).toBe(200)
+    expect(response.text).toBe('halo jamaluddin')
     expect(response.get("content-type")).toContain('text/plain')
 })
+
+// app.use("/jamal", router2)
+// Artinya: semua route di dalam router2 otomatis ditambahkan prefix /jamal.
+// jadi , /jamal akan menjadi /jamal/jamal
+// kalo mau request(app).get("/jamal/jamal")
+
+// Namun, kamu juga sudah menuliskan router2.get('/jamal', ...), sehingga route akhirnya menjadi: /jamal/jamal
+
+
+// atau kamu mau juga bisa gini 
+// router2.get('/', (req, res) => {
+//     res.send(`halo ${req.nama}`);
+// });
+
+// app.use("/jamal", router2);
