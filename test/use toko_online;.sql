9766e4478457ec5479d8aa77466ae32c8633b02c
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-07-09 10:06:46 +0700

    test

---
+++ test/use toko_online;.sql
@@ -0,0 +1,11 @@
+use toko_online;
+select * from products;
+-- Ctrl+Alt+E
+
+-- jalankan ini di terminal
+-- kalo au ganti lagi, nanti ganti lagi aja
+ALTER USER 'root'@'localhost'
+IDENTIFIED WITH mysql_native_password BY '1234';
+FLUSH PRIVILEGES;
+
+SELECT user, host, plugin FROM mysql.user;
